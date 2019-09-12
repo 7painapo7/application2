065c9f7cda22bf888d46a2bdf625eed687dc4dbd
にしゆうき <nishiyuki@nishiyuukinoMacBook-Air.local>
2019-09-12 16:27:06 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a687d98ea5236e159d2f0bbe96412be8537fa105dd7cd3d569089fbcd0cc35aa9007d20f1c27ca694b67f1e00427690a79825387279b30a81365471b866a826c'
